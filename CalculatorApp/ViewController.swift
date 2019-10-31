f9ead02148f341e1c39a68f0f3b9c2e074f02ce0
Bri Hewlett <bhewlett00@gmail.com>
2019-10-31 10:02:26 -0400

    added basic calculator functions

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amount1TextField: UITextField!
+    
+    
+    @IBOutlet weak var amount2TextField: UITextField!
+    
+    @IBOutlet weak var calculationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addValues(_ sender: Any) {
+        var dTotal = 0.0
+        var dAmount1 = 0.0
+        var dAmount2 = 0.0
+        
+        if let amount1 = amount1TextField.text, let amount2 = amount2TextField.text{
+            let trimmedAmount1 = amount1.trimmingCharacters(in: .whitespaces)
+            let trimmedAmount2 = amount2.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedAmount1.isEmpty && !trimmedAmount2.isEmpty){
+                dAmount1 = Double(trimmedAmount1)!
+                dAmount2 = Double(trimmedAmount2)!
+                
+                dTotal = dAmount1 + dAmount2
+            }
+        }
+        calculationLabel.text = String(dTotal)
+    }
+    
+    @IBAction func subtractValues(_ sender: Any) {
+        var dTotal = 0.0
+        var dAmount1 = 0.0
+        var dAmount2 = 0.0
+        
+        if let amount1 = amount1TextField.text, let amount2 = amount2TextField.text{
+            let trimmedAmount1 = amount1.trimmingCharacters(in: .whitespaces)
+            let trimmedAmount2 = amount2.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedAmount1.isEmpty && !trimmedAmount2.isEmpty){
+                dAmount1 = Double(trimmedAmount1)!
+                dAmount2 = Double(trimmedAmount2)!
+                
+                dTotal = dAmount1 - dAmount2
+            }
+        }
+        calculationLabel.text = String(dTotal)
+    }
+    
+    @IBAction func multiplyValues(_ sender: Any) {
+        var dTotal = 0.0
+        var dAmount1 = 0.0
+        var dAmount2 = 0.0
+        
+        if let amount1 = amount1TextField.text, let amount2 = amount2TextField.text{
+            let trimmedAmount1 = amount1.trimmingCharacters(in: .whitespaces)
+            let trimmedAmount2 = amount2.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedAmount1.isEmpty && !trimmedAmount2.isEmpty){
+                dAmount1 = Double(trimmedAmount1)!
+                dAmount2 = Double(trimmedAmount2)!
+                
+                dTotal = dAmount1 * dAmount2
+            }
+        }
+        calculationLabel.text = String(dTotal)
+    }
+    
+    
+    @IBAction func divideValues(_ sender: Any) {
+        var dTotal = 0.0
+        var dAmount1 = 0.0
+        var dAmount2 = 0.0
+        
+        if let amount1 = amount1TextField.text, let amount2 = amount2TextField.text{
+            let trimmedAmount1 = amount1.trimmingCharacters(in: .whitespaces)
+            let trimmedAmount2 = amount2.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedAmount1.isEmpty && !trimmedAmount2.isEmpty){
+                dAmount1 = Double(trimmedAmount1)!
+                dAmount2 = Double(trimmedAmount2)!
+                
+                dTotal = dAmount1 / dAmount2
+            }
+        }
+        calculationLabel.text = String(dTotal)
+    }
 }
 
